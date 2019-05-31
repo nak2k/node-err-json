8b69ec8d894f846d3cd55115103dd26d99ebf84d
Kengo Nakatsuka <kengo.nakatsuka@gmail.com>
2019-05-31 12:42:12 +0900

    Improve handling non error values

--- test/test.js
+++ test/test.js
@@ -4,7 +4,7 @@ const {
   errJsonDebug,
 } = require('..');
 
-test('test errJson()', t => {
+test('test errJson(err)', t => {
   t.plan(1);
 
   const err = new Error('test');
@@ -20,7 +20,20 @@ test('test errJson()', t => {
   });
 });
 
-test('test errJsonDebug()', t => {
+test('test errJson(123)', t => {
+  t.plan(1);
+
+  const json = errJson(123);
+
+  t.deepEqual(json, {
+    name: 'Error',
+    message: 'The err must be an instance of Error',
+    code: undefined,
+    err: 123,
+  });
+});
+
+test('test errJsonDebug(err)', t => {
   t.plan(4);
 
   const err = new Error('test');
@@ -32,4 +45,40 @@ test('test errJsonDebug()', t => {
   t.equal(json.message, 'test');
   t.equal(json.foo, 'foo');
   t.equal(typeof(json.stack), 'string');
+});
+
+test('test errJsonDebug(123)', t => {
+  t.plan(4);
+
+  const json = errJsonDebug(123);
+
+  t.equal(json.name, 'Error');
+  t.equal(json.message, 'The err must be an instance of Error');
+  t.equal(json.err, 123);
+  t.equal(typeof(json.stack), 'string');
+});
+
+test('test errJsonDebug(err, true)', t => {
+  t.plan(4);
+
+  const err = new Error('test');
+  err.foo = 'foo';
+
+  const json = errJsonDebug(err, true);
+
+  t.equal(json.name, 'Error');
+  t.equal(json.message, 'test');
+  t.equal(json.foo, 'foo');
+  t.ok(Array.isArray(json.stack));
+});
+
+test('test errJsonDebug(123, true)', t => {
+  t.plan(4);
+
+  const json = errJsonDebug(123, true);
+
+  t.equal(json.name, 'Error');
+  t.equal(json.message, 'The err must be an instance of Error');
+  t.equal(json.err, 123);
+  t.ok(Array.isArray(json.stack));
 });
